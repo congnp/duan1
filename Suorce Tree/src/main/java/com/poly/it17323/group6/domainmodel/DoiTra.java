81eef0d4ed86599b14ff32cfcd958abca10c045b
PNDanh <danhpnph26382@fpt.edu.vn>
2022-11-17 10:04:01 +0700

    Revert "Revert "CRUD: anh, chatlieu, chitietsanpham, chucvu, doitra, hoadon""
    This reverts commit 6d188f7bab14473e3f4e69fe186648b35601e65d.

---
+++ Suorce Tree/src/main/java/com/poly/it17323/group6/domainmodel/DoiTra.java
@@ -0,0 +1,50 @@
+package com.poly.it17323.group6.domainmodel;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author pdanh
+ */
+@Entity
+@Table(name = "DoiTra")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DoiTra implements Serializable {
+
+    @Id
+    @Column(name = "IdDoiTra")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String idDT;
+
+    @Column(name = "LyDoDoiTra")
+    private String lyDoDT;
+
+    @Column(name = "NgayDoiTra")
+    private Date ngayDT;
+
+    @Column(name = "HanDoiTra")
+    private Date hanDT;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IDHD", referencedColumnName = "IdHD")
+    private HoaDon hoaDon;
+}
---
+++ Suorce Tree/src/main/java/com/poly/it17323/group6/domainmodel/HoaDon.java
@@ -0,0 +1,64 @@
+package com.poly.it17323.group6.domainmodel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author pdanh
+ */
+@Entity
+@Table(name = "HoaDon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class HoaDon implements Serializable {
+
+    @Id
+    @Column(name = "IdHD")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String idHD;
+
+    @Column(name = "MaHD")
+    private String maHD;
+
+    @Column(name = "PhuongThucTT")
+    private Integer pttt;
+
+    @Column(name = "TongTien")
+    private BigDecimal tongTien;
+
+    @Column(name = "TinhTrang")
+    private Integer tinhTrang;
+
+    @Column(name = "NgayTao")
+    private String ngayTao;
+
+    @Column(name = "NgaySua")
+    private String ngaySua;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IdND", referencedColumnName = "Id")
+    private NguoiDung nguoiDung;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IdKH", referencedColumnName = "IdKH")
+    private KhachHang khachHang;
+}
