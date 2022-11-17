23654596761aa9644cc63746861b8e8aeb230c8f
PNDanh <danhpnph26382@fpt.edu.vn>
2022-11-17 12:01:45 +0700

    CRUD

--- src/main/java/com/poly/it17323/group6/repository/AnhRepository.java
+++ src/main/java/com/poly/it17323/group6/repository/AnhRepository.java
@@ -1,40 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.poly.it17323.group6.repository;
 
-import com.poly.it17323.group6.domainmodel.HoaDon;
+import com.poly.it17323.group6.domainmodel.Anh;
 import com.poly.it17323.group6.hibernateconfig.Hibernate_Util;
 import java.util.List;
 import javax.persistence.Query;
+import javax.transaction.Transaction;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 /**
  *
- * @author pdanh
+ * @author ThanhNam
  */
-public class HoaDonRepository {
+public class AnhRepository {
 
     private Session session = Hibernate_Util.getFACTORY().openSession();
-    private String fromTable = "From HoaDon";
+    private String fromTable = "From Anh";
 
-    public List<HoaDon> getAll() {
-        Query query = session.createQuery(fromTable, HoaDon.class);
-        List<HoaDon> list = query.getResultList();
+    public List<Anh> getAll() {
+        Query query = session.createQuery(fromTable, Anh.class);
+        List<Anh> list = query.getResultList();
         return list;
     }
 
-    public HoaDon getOne(Integer id) {
+    public Anh getOne(Integer id) {
         String sql = fromTable + "Where id =: id";
-        Query query = session.createQuery(fromTable, HoaDon.class);
+        Query query = session.createQuery(fromTable, Anh.class);
         query.setParameter("id", id);
-        HoaDon hoaDon = (HoaDon) query.getSingleResult();
-        return hoaDon;
+        Anh anh = (Anh) query.getSingleResult();
+        return anh;
     }
 
-    public Boolean add(HoaDon hoaDon) {
+    public Boolean add(Anh anh) {
         Transaction transaction = null;
         try {
             transaction = (Transaction) session.beginTransaction();
-            session.save(hoaDon);
+            session.save(anh);
             transaction.commit();
             return true;
         } catch (Exception e) {
@@ -43,11 +47,11 @@ public class HoaDonRepository {
         return null;
     }
 
-    public Boolean update(HoaDon hoaDon) {
+    public Boolean update(Anh anh) {
         Transaction transaction = null;
         try {
             transaction = (Transaction) session.beginTransaction();
-            session.saveOrUpdate(hoaDon);
+            session.saveOrUpdate(anh);
             transaction.commit();
             return true;
         } catch (Exception e) {
@@ -56,11 +60,11 @@ public class HoaDonRepository {
         return null;
     }
 
-    public Boolean delete(HoaDon hoaDon) {
+    public Boolean delete(Anh anh) {
         Transaction transaction = null;
         try {
             transaction = (Transaction) session.beginTransaction();
-            session.delete(hoaDon);
+            session.delete(anh);
             transaction.commit();
             return true;
         } catch (Exception e) {
@@ -69,10 +73,4 @@ public class HoaDonRepository {
         return null;
     }
 
-    public static void main(String[] args) {
-        List<HoaDon> list = new HoaDonRepository().getAll();
-        for (HoaDon hoaDon : list) {
-            System.out.println(hoaDon.toString());
-        }
-    }
 }
