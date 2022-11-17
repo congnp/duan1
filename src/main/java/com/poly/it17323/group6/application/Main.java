a75016182bb4610d8340fe1395709cd428008c89
PNDanh <danhpnph26382@fpt.edu.vn>
2022-11-17 12:39:29 +0700

    Project : DomainModel + DB + HibernateConfig Done

---
+++ src/main/java/com/poly/it17323/group6/application/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.it17323.group6.application;
+
+import com.poly.it17323.group6.view.SRM_BanHang;
+
+/**
+ *
+ * @author pdanh
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new SRM_BanHang().setVisible(true);
+    }
+}
---
+++ src/main/java/com/poly/it17323/group6/domainmodel/Anh.java
@@ -0,0 +1,49 @@
+package com.poly.it17323.group6.domainmodel;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author pdanh
+ */
+@Entity
+@Table(name = "Anh")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Anh implements Serializable {
+
+    @Id
+    @Column(name = "IdAnh")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String idAnh;
+
+    @Column(name = "MaAnh")
+    private String maAnh;
+
+    @Column(name = "TenAnh")
+    private String tenAnh;
+
+    @Column(name = "DuongDanAnh")
+    private String linkAnh;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "IDCTSP", referencedColumnName = "IdCTSP")
+    private ChiTietSanPham chiTietSanPham;
+}
