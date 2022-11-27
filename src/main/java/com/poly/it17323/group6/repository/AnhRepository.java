14288c3dfa70ab190b687fa2b0f359473554375f
ADMIN\Admin <vannntph26342@fpt.edu.vn>
2022-11-27 21:20:59 +0700

    Sửa CRUD Sản Phảm

--- src/main/java/com/poly/it17323/group6/repository/AnhRepository.java
+++ src/main/java/com/poly/it17323/group6/repository/AnhRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import javax.persistence.Query;
-import javax.transaction.Transaction;
+import org.hibernate.Transaction;
 import org.hibernate.Session;
 
 /**
